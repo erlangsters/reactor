eedd4743466ca395544585520ab370b912fde7b7
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2025-01-05 07:34:03 +0800

    Implement reactor behavior (no unit test)

--- src/reactor.erl
+++ src/reactor.erl
@@ -8,7 +8,321 @@
 %% Written by Jonathan De Wachter <jonathan.dewachter@byteplug.io>, November 2024
 %%
 -module(reactor).
--export([foobar/0]).
 
-foobar() ->
+-export([spawn/3, spawn/4, spawn/5]).
+-export([request/2, request/3]).
+-export([notify/2]).
+-export([reply/2]).
+-export([get_state/1]).
+-export([get_data/1]).
+
+-compile({no_auto_import, [spawn/4]}).
+
+%%
+%% The reactor behavior.
+%%
+%% To be written.
+%%
+
+-type name() :: atom().
+-type id() :: name() | pid().
+
+-type timer_action() :: {timer, timeout(), Message :: term()}.
+-type task_action() :: {task, Message :: term()}.
+-type action() :: timer_action() | task_action().
+
+-type request_id() :: erlang:timestamp().
+-type from() :: {pid(), request_id()}.
+
+-type spawn_return() ::
+    already_spawned |
+    {ok, spawner:spawn_return()} |
+    {aborted, Reason :: term()} |
+    timeout |
+    {error, Reason :: term()}
+.
+
+-callback initialize(Args :: [term()]) ->
+    {continue, State :: atom(), Data :: term()} |
+    {continue, State :: atom(), Data :: term(), Action :: action()} |
+    {abort, Reason :: term()}
+.
+
+-callback handle_request(State :: atom(), Request :: term(), From :: from(), Data :: term()) ->
+    {reply, Response :: term(), NewState :: atom(), NewData :: term()} |
+    {reply, Response :: term(), NewState :: atom(), NewData :: term(), Action :: action()} |
+    {no_reply, NewData :: term(), NewState :: atom()} |
+    {no_reply, NewData :: term(), NewState :: atom(), Action :: action()} |
+    {stop, Reason :: term(), Reply :: term(), NewData :: term()} |
+    {stop, Reason :: term(), NewData :: term()}
+.
+
+-callback handle_notification(State :: atom(), Notification :: term(), Data :: term()) ->
+    {continue, NewState :: atom(), NewData :: term()} |
+    {continue, NewState :: atom(), NewData :: term(), Action :: action()} |
+    {stop, Reason :: term(), NewData :: term()}
+.
+
+-callback handle_message(State :: atom(), Message :: term(), Data :: term()) ->
+    {continue, NewState :: atom(), NewData :: term()} |
+    {continue, NewState :: atom(), NewData :: term(), Action :: action()} |
+    {stop, Reason :: term(), NewData :: term()}
+.
+
+-callback handle_task(State :: atom(), Payload :: term(), Data :: term()) ->
+    {continue, NewState :: atom(), NewData :: term()} |
+    {continue, NewState :: atom(), NewData :: term(), Action :: action()} |
+    {stop, Reason :: term(), NewData :: term()}
+.
+
+-callback handle_timeout(State :: atom(), Payload :: term(), Data :: term()) ->
+    {continue, NewState :: atom(), NewData :: term()} |
+    {continue, NewState :: atom(), NewData :: term(), Action :: action()} |
+    {stop, Reason :: term(), NewData :: term()}
+.
+
+-callback handle_transition(State :: atom(), NewState :: atom(), Data :: term()) ->
+    {continue, NewData :: term()} |
+    {continue, NewData :: term(), Action :: action()} |
+    {stop, Reason :: term(), NewData :: term()}.
+
+-callback terminate(State :: atom(), Reason :: term(), Data :: term()) ->
+    no_return().
+
+-spec spawn(spawner:mode(), module(), [term()]) -> spawn_return().
+spawn(Mode, Module, Args) ->
+    spawn(Mode, no_name, Module, Args).
+
+-spec spawn(spawner:mode(), no_name | {name, name()}, module(), [term()]) ->
+    spawn_return().
+spawn(Mode, Name, Module, Args) ->
+    spawn(Mode, Name, Module, Args, infinity).
+
+-spec spawn(
+    spawner:mode(),
+    no_name | {name, name()},
+    module(),
+    [term()],
+    timeout()
+) -> spawn_return().
+spawn(Mode, Name, Module, Args, Timeout) ->
+    Root = self(),
+    Loop = fun() ->
+        % We register the process (if requested).
+        Return = case Name of
+            no_name ->
+                register_ok;
+            {name, Name_} ->
+                try register(Name_, self()) of
+                    true ->
+                        register_ok
+                catch
+                    error:badarg ->
+                        register_not_ok
+                end
+        end,
+        case Return of
+            register_ok ->
+                case Module:initialize(Args) of
+                    {continue, State, Data} ->
+                        Root ! {self(), '$reactor_initialized'},
+                        loop(Module, State, State, Data, no_action, []);
+                    {continue, State, Data, Action} ->
+                        Root ! {self(), '$reactor_initialized'},
+                        loop(Module, State, State, Data, Action, []);
+                    {abort, Reason} ->
+                        Root ! {self(), '$reactor_aborted', Reason}
+                end;
+            register_not_ok ->
+                Root ! {self(), '$reactor_already_registered'}
+        end
+    end,
+    Setup = fun(Pid, Monitor) ->
+        receive
+            {'DOWN', Monitor, process, Pid, Reason} ->
+                {error, Reason};
+            {Pid, '$reactor_already_registered'} ->
+                reactor_already_registered;
+            {Pid, '$reactor_initialized'} ->
+                ok;
+            {Pid, '$reactor_aborted', Reason} ->
+                {abort, Reason}
+        after Timeout ->
+            % XXX: Fix race condition here.
+            erlang:exit(Pid, kill),
+            timeout
+        end
+    end,
+    {setup, Value, Return} = spawner:setup_spawn(Mode, Loop, Setup),
+    case Value of
+        ok ->
+            {ok, Return};
+        reactor_already_registered ->
+            already_spawned;
+        {abort, Reason} ->
+            {aborted, Reason};
+        timeout ->
+            timeout;
+        {error, Reason} ->
+            {error, Reason}
+    end.
+
+-spec request(id(), term()) -> {reply, Response :: term()} | no_reply.
+request(Server, Request) ->
+    request(Server, Request, infinity).
+
+-spec request(id(), term(), timeout()) -> {reply, Reply :: term()} | no_reply.
+request(Server, Request, Timeout) ->
+    RequestId = erlang:timestamp(),
+    From = {self(), RequestId},
+
+    Server ! {'$reactor_request', From, Request},
+    receive
+        {reply, RequestId, Response} ->
+            {reply, Response}
+    after Timeout ->
+        no_reply
+    end.
+
+-spec notify(id(), term()) -> ok.
+notify(Server, Message) ->
+    Server ! {'$reactor_notify', Message},
     ok.
+
+-spec reply(from(), term()) -> ok.
+reply({Pid, RequestId}, Response) ->
+    Pid ! {reply, RequestId, Response},
+    ok.
+
+-spec get_state(id()) -> atom().
+get_state(Server) ->
+    Server ! {'$reactor_state', self()},
+    receive
+        {'$reactor_state', Server, State} ->
+            State
+    end.
+
+-spec get_data(id()) -> term().
+get_data(Server) ->
+    Server ! {'$reactor_data', self()},
+    receive
+        {'$reactor_data', Server, Data} ->
+            Data
+    end.
+
+loop(Module, State, State, Data, {timer, Timeout, Message}, Timers) ->
+    % We must create a timer that must be ignored if we receive a message
+    % before it expires. We generate a unique ID in order to identify it.
+    Id = erlang:timestamp(),
+    erlang:send_after(Timeout, self(), {'$reactor_timeout', Id, Message}),
+    loop(Module, State, State, Data, no_action, [Id|Timers]);
+loop(Module, State, State, Data, {task, Task}, Timers) ->
+    % We must execute a task.
+    case Module:handle_task(State, Task, Data) of
+        {continue, NewState} ->
+            loop(Module, State, NewState, Data, no_action, Timers);
+        {continue, NewState, Action} ->
+            loop(Module, State, NewState, Data, Action, Timers);
+        {stop, Reason, NewData} ->
+            try_terminate(Module, State, Reason, NewData)
+    end;
+loop(Module, State, State, Data, no_action, Timers) ->
+    receive
+        {'$reactor_state', Pid} ->
+            Pid ! {'$reactor_state', self(), State},
+            loop(Module, State, State, Data, no_action, Timers);
+        {'$reactor_data', Pid} ->
+            Pid ! {'$reactor_data', self(), Data},
+            loop(Module, State, State, Data, no_action, Timers);
+        {'$reactor_request', From, Request} ->
+            % We clear the list of timers in order to ignore timers that are
+            % currently active.
+            NewTimers = [],
+
+            case Module:handle_request(State, Request, From, Data) of
+                {reply, Reply, NewState, NewData} ->
+                    ok = reply(From, Reply),
+                    loop(Module, State, NewState, NewData, no_action, NewTimers);
+                {reply, Reply, NewState, NewData, Action} ->
+                    ok = reply(From, Reply),
+                    loop(Module, State, NewState, NewData, Action, NewTimers);
+                {no_reply, NewState, NewData} ->
+                    loop(Module, State, NewState, NewData, no_action, NewTimers);
+                {no_reply, NewState, NewData, Action} ->
+                    loop(Module, State, NewState, NewData, Action, NewTimers);
+                {stop, Reason, Reply, NewData} ->
+                    ok = reply(From, Reply),
+                    try_terminate(Module, State, Reason, NewData);
+                {stop, Reason, NewData} ->
+                    try_terminate(Module, State, Reason, NewData)
+            end;
+        {'$reactor_notify', Message} ->
+            % We clear the list of timers in order to ignore timers that are
+            % currently active.
+            NewTimers = [],
+
+            case Module:handle_notification(State, Message, Data) of
+                {continue, NewState, NewData} ->
+                    loop(Module, State, NewState, NewData, no_action, NewTimers);
+                {continue, NewState, NewData, Action} ->
+                    loop(Module, State, NewState, NewData, Action, NewTimers);
+                {stop, Reason, NewData} ->
+                    try_terminate(Module, State, Reason, NewData)
+            end;
+        {'$reactor_timeout', Id, Message} ->
+            % A timer has expired but we must ignore it if a message has been
+            % received. If the timer ID is in the list of timers, we don't
+            % ignore it.
+            case lists:member(Id, Timers) of
+                true ->
+                    % Remove the timer ID from the list of timers.
+                    NewTimers = lists:delete(Id, Timers),
+
+                    case Module:handle_timeout(State, Message, Data) of
+                        {continue, NewState, NewData} ->
+                            loop(Module, State, NewState, NewData, no_action, NewTimers);
+                        {continue, NewState, NewData, Action} ->
+                            loop(Module, State, NewState, NewData, Action, NewTimers);
+                        {stop, Reason, NewData} ->
+                            try_terminate(Module, State, Reason, NewData)
+                    end;
+                false ->
+                    % Simply ignore the timer.
+                    loop(Module, State, State, Data, no_action, Timers)
+            end;
+        '$reactor_stop' ->
+            try_terminate(Module, State, normal, Data);
+        Message ->
+            % We clear the list of timers in order to ignore timers that are
+            % currently active.
+            NewTimers = [],
+
+            case Module:handle_message(State, Message, Data) of
+                {continue, NewState, NewData} ->
+                    loop(Module, State, NewState, NewData, no_action, NewTimers);
+                {continue, NewState, NewData, Action} ->
+                    loop(Module, State, NewState, NewData, Action, NewTimers);
+                {stop, Reason, NewData} ->
+                    try_terminate(Module, State, Reason, NewData)
+            end
+    end;
+loop(Module, State, NewState, Data, _Action, _Timers) ->
+    % A state change has occurred. (We clear the list of timers in order to
+    % ignore them. The action is also ignored.)
+    case Module:handle_transition(State, NewState, Data) of
+        {continue, NewData} ->
+            loop(Module, NewState, NewState, NewData, no_action, []);
+        {continue, NewData, NewAction} ->
+            loop(Module, NewState, NewState, NewData, NewAction, []);
+        {stop, Reason, NewData} ->
+            try_terminate(Module, State, Reason, NewData)
+    end.
+
+try_terminate(Module, State, Reason, Data) ->
+    case erlang:function_exported(Module, terminate, 3) of
+        true ->
+            Module:terminate(State, Reason, Data),
+            ok;
+        false ->
+            ok
+    end.
--- test/reactor_test.erl
+++ test/reactor_test.erl
@@ -10,5 +10,131 @@
 -module(reactor_test).
 -include_lib("eunit/include/eunit.hrl").
 
+compute_result(Operation, A, B) ->
+    case Operation of
+        add -> A + B;
+        subtract -> A - B;
+        multiply -> A * B
+    end.
+
 reactor_test() ->
+    % A canonical test meant to verify the key features of the behavior.
+    meck:new(my_reactor, [non_strict]),
+    meck:expect(my_reactor, initialize, fun([A, B]) ->
+        {continue, add, #{a => A, b => B}}
+    end),
+    meck:expect(my_reactor, handle_request, fun
+        (Operation, result, _From, #{a := A, b := B} = Data) ->
+            {reply, compute_result(Operation, A, B), Operation, Data};
+        (_Operation, switch, _From, _Data) ->
+            % Move to 'subtract' state and switch the operands.
+            {reply, ok, subtract, #{a => 69, b => 42}}
+    end),
+    meck:expect(my_reactor, handle_notification, fun
+        (Operation, {Pid, result}, #{a := A, b := B} = Data) ->
+            Pid ! {self(), compute_result(Operation, A, B)},
+            {continue, Operation, Data};
+        (_Operation, switch, _Data) ->
+            % Move to 'multiply' state and switch the operands.
+            {continue, multiply, #{a => 10, b => 100}}
+    end),
+    meck:expect(my_reactor, handle_message, fun
+        (Operation, {Pid, result}, #{a := A, b := B} = Data) ->
+            Pid ! {self(), compute_result(Operation, A, B)},
+            {continue, Operation, Data};
+        (_Operation, switch, _Data) ->
+            % Move back to initial state and data.
+            {continue, add, #{a => 42, b => 69}}
+    end),
+    Root = self(),
+    meck:expect(my_reactor, handle_transition, fun(From, To, Data) ->
+        Root ! {transition, From, To, Data},
+        {continue, Data}
+    end),
+
+    {ok, Pid} = reactor:spawn(link, my_reactor, [42, 69]),
+
+    {reply, 111} = reactor:request(Pid, result),
+    reactor:notify(Pid, {self(), result}),
+    ok = receive
+        {Pid, 111} -> ok
+    end,
+    Pid ! {self(), result},
+    ok = receive
+        {Pid, 111} -> ok
+    end,
+
+    {reply, ok} = reactor:request(Pid, switch),
+
+    ok = receive
+        {transition, add, subtract, #{a := 69, b := 42}} ->
+            ok
+    end,
+
+    {reply, 27} = reactor:request(Pid, result),
+    reactor:notify(Pid, {self(), result}),
+    ok = receive
+        {Pid, 27} -> ok
+    end,
+    Pid ! {self(), result},
+    ok = receive
+        {Pid, 27} -> ok
+    end,
+
+    ok = reactor:notify(Pid, switch),
+    ok = receive
+        {transition, subtract, multiply, #{a := 10, b := 100}} ->
+            ok
+    end,
+
+    {reply, 1000} = reactor:request(Pid, result),
+    reactor:notify(Pid, {self(), result}),
+    ok = receive
+        {Pid, 1000} -> ok
+    end,
+    Pid ! {self(), result},
+    ok = receive
+        {Pid, 1000} -> ok
+    end,
+
+    Pid ! switch,
+    ok = receive
+        {transition, multiply, add, #{a := 42, b := 69}} ->
+            ok
+    end,
+
+    meck:unload(my_reactor),
+
+    ok.
+
+reactor_spawn_test() ->
+    % XXX: To be implemented.
+    ok.
+
+reactor_initialize_test() ->
+    % XXX: To be implemented.
+    ok.
+
+reactor_handle_request_test() ->
+    % XXX: To be implemented.
+    ok.
+
+reactor_handle_notification_test() ->
+    % XXX: To be implemented.
+    ok.
+
+reactor_handle_message_test() ->
+    % XXX: To be implemented.
+    ok.
+
+reactor_handle_timeout_test() ->
+    % XXX: To be implemented.
+    ok.
+
+reactor_handle_task_test() ->
+    % XXX: To be implemented.
+    ok.
+
+reactor_handle_transition_test() ->
+    % XXX: To be implemented.
     ok.
